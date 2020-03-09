058a688b1892d4af1a08487bcb6741146a84e1ed
Sato Yuya <satoy@mediowl.co.jp>
2020-03-09 13:51:37 +0900

    add faker

--- database/factories/TaskFactory.php
+++ database/factories/TaskFactory.php
@@ -5,12 +5,12 @@
 use App\Model;
 use Faker\Generator as Faker;
 
-$factory->define(Model::class, function (Faker $faker) {
+$factory->define(App\Task::class, function (Faker $faker) {
     return [
         'user_id' => 6,
         'title' => $faker->word,
         'content' => $faker->sentence,
         'status' => $faker->numberBetween($min = 1, $max = 3),
-        'due_date' => $faker->dateTime($min = 'now')->format('Y-m-d'),
+        'due_date' => $faker->date($format='Y-m-d',$min='now'),
     ];
 });
